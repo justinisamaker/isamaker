1306a4b9270e6f249b7e27648afe0842e3d20480
Justin Smith <justin@isamaker.com>
2019-11-04 15:52:30 -0600

    Update post header column spacing

--- src/components/postheader/PostHeader.js
+++ src/components/postheader/PostHeader.js
@@ -5,12 +5,14 @@ class PostHeader extends React.Component{
   render(){
     return(
       <header className="post-content-header">
+        <div>
+          <h1>{this.props.title}</h1>
+          <p className="meta">{this.props.date}</p>
+        </div>
         { this.props.link ?
           <a href={this.props.link} className="post-external-link" target="_new">View the live version of {this.props.title} <span class="right-arrow">&raquo;</span></a>
           : null
         }
-        <h1>{this.props.title}</h1>
-        <p className="meta">{this.props.date}</p>
       </header>
     );
   }
