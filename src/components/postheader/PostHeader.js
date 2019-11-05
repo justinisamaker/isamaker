0eab29d8a08ce6a831fcb216fda3043efb0e69f4
Justin Smith <justin@isamaker.com>
2019-11-05 14:52:18 -0600

    Responsive design for internal pages

--- src/components/postheader/PostHeader.js
+++ src/components/postheader/PostHeader.js
@@ -10,7 +10,7 @@ class PostHeader extends React.Component{
           <p className="meta">{this.props.date}</p>
         </div>
         { this.props.link ?
-          <a href={this.props.link} className="post-external-link" target="_new">View the live version of {this.props.title} <span class="right-arrow">&raquo;</span></a>
+          <a href={this.props.link} className="post-external-link" target="_new">View the live version <br className="external-link-break"/>of this project <span class="right-arrow">&raquo;</span></a>
           : null
         }
       </header>
