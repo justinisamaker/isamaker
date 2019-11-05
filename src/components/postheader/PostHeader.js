81a3425ee3cd152e6f54c37f54aed2a4df4af7cc
Justin Smith <justin@isamaker.com>
2019-11-05 16:09:38 -0600

    Revert "Ran the formatter to clean things up"
    This reverts commit bbbd4fd1b3d9fba2d3d452477429fc5a345f8ea0.

--- src/components/postheader/PostHeader.js
+++ src/components/postheader/PostHeader.js
@@ -1,27 +1,21 @@
-import React from 'react'
-import './PostHeader.scss'
+import React from 'react';
+import './PostHeader.scss';
 
-class PostHeader extends React.Component {
-  render() {
-    return (
+class PostHeader extends React.Component{
+  render(){
+    return(
       <header className="post-content-header">
         <div>
           <h1>{this.props.title}</h1>
           <p className="meta">{this.props.date}</p>
         </div>
-        {this.props.link ? (
-          <a
-            href={this.props.link}
-            className="post-external-link"
-            target="_new"
-          >
-            View the live version <br className="external-link-break" />
-            of this project <span class="right-arrow">&raquo;</span>
-          </a>
-        ) : null}
+        { this.props.link ?
+          <a href={this.props.link} className="post-external-link" target="_new">View the live version <br className="external-link-break"/>of this project <span class="right-arrow">&raquo;</span></a>
+          : null
+        }
       </header>
-    )
+    );
   }
 }
 
-export default PostHeader
+export default PostHeader;
