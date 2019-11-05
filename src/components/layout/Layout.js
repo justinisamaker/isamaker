c793d6b2d48476af22b5dd7f3e587174581abb30
Justin Smith <justin@isamaker.com>
2019-11-05 16:11:16 -0600

    Eh, keep changing my mind on formatting

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,23 +1,22 @@
-import React from 'react';
-import { Link } from 'gatsby';
-import Sidebar from '../sidebar/Sidebar';
+import React from 'react'
+import { Link } from 'gatsby'
+import Sidebar from '../sidebar/Sidebar'
 
-import '../main.scss';
-import './Layout.scss';
+import '../main.scss'
+import './Layout.scss'
 
 class Layout extends React.Component {
   render() {
-    const { location, title, children } = this.props;
-    const rootPath = `${__PATH_PREFIX__}/`;
+    const { location, title, children } = this.props
+    const rootPath = `${__PATH_PREFIX__}/`
 
-    let contentContainer = (location.pathname === rootPath ? "home-content" : "post-content")
+    let contentContainer =
+      location.pathname === rootPath ? 'home-content' : 'post-content'
 
     return (
       <div className="container">
         <Sidebar siteTitle={title} />
-        <div className={contentContainer}>
-          {children}
-        </div>
+        <div className={contentContainer}>{children}</div>
       </div>
     )
   }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,61 +1,73 @@
-import React from 'react';
-import { graphql } from 'gatsby';
-import TransitionLink from 'gatsby-plugin-transition-link';
-import AniLink from 'gatsby-plugin-transition-link/AniLink';
+import React from 'react'
+import { graphql } from 'gatsby'
+import TransitionLink from 'gatsby-plugin-transition-link'
+import AniLink from 'gatsby-plugin-transition-link/AniLink'
 
-import Layout from '../components/layout/Layout';
-import SEO from '../components/seo';
-import '../components/home.scss';
+import Layout from '../components/layout/Layout'
+import SEO from '../components/seo'
+import '../components/home.scss'
 
 class BlogIndex extends React.Component {
   render() {
-    const { data } = this.props;
-    const siteTitle = data.site.siteMetadata.title;
-    const posts = data.allMdx.edges;
+    const { data } = this.props
+    const siteTitle = data.site.siteMetadata.title
+    const posts = data.allMdx.edges
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
-
-        <SEO title="Justin is a Maker" keywords={[
-          'creative technologist',
-          'Chicago',
-          'New Orleans',
-          'react',
-          'front-end',
-          'front end',
-          'javascript',
-          'raspberry pi',
-          'arduino'
-        ]} />
+        <SEO
+          title="Justin is a Maker"
+          keywords={[
+            'creative technologist',
+            'Chicago',
+            'New Orleans',
+            'react',
+            'front-end',
+            'front end',
+            'javascript',
+            'raspberry pi',
+            'arduino',
+          ]}
+        />
 
         {posts.map(({ node }) => {
-          const title = node.frontmatter.title || node.fields.slug;
-          let image = node.frontmatter.featuredImage.childImageSharp.fluid.src;
+          const title = node.frontmatter.title || node.fields.slug
+          let image = node.frontmatter.featuredImage.childImageSharp.fluid.src
           return (
-            <AniLink cover bg="url(/stardust2.png) repeat" direction="left" key={node.fields.slug} to={node.fields.slug} className="project">
+            <AniLink
+              cover
+              bg="url(/stardust2.png) repeat"
+              direction="left"
+              key={node.fields.slug}
+              to={node.fields.slug}
+              className="project"
+            >
               <div className="project-title">
                 <h2>{title}</h2>
               </div>
 
               <div className="project-info">
                 <h3>{title}</h3>
-                <p dangerouslySetInnerHTML={{ __html: node.frontmatter.teaser }} />
+                <p
+                  dangerouslySetInnerHTML={{ __html: node.frontmatter.teaser }}
+                />
               </div>
 
               <div className="project-overlay"></div>
 
-              <div className="project-image"
-                style={{ backgroundImage: `url(${image})` }}>
-              </div>
+              <div
+                className="project-image"
+                style={{ backgroundImage: `url(${image})` }}
+              ></div>
             </AniLink>
-          );
+          )
         })}
       </Layout>
     )
   }
 }
 
-export default BlogIndex;
+export default BlogIndex
 
 export const pageQuery = graphql`
   query {
@@ -76,8 +88,8 @@ export const pageQuery = graphql`
             title
             teaser
             featuredImage {
-              childImageSharp{
-                fluid(maxWidth:1200){
+              childImageSharp {
+                fluid(maxWidth: 1200) {
                   ...GatsbyImageSharpFluid
                 }
               }
@@ -87,4 +99,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`;
+`
