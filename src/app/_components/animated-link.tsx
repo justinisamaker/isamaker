96d6dfd5e003ad33879b12a5f94a7d1ba131c04d
Justin Smith <justin@isamaker.com>
2026-06-16 18:22:03 -0500

    feat: add className to animateLink

--- src/app/_components/animated-link.tsx
+++ src/app/_components/animated-link.tsx
@@ -1,15 +1,17 @@
 export default function AnimatedLink({
   href,
   children,
+  className,
 }: {
   href: string;
   children: string;
+  className?: string;
 }) {
   return (
     <a
       href={href}
       target="_new"
-      className="hover:text-blue-300 transition-colors duration-200 group"
+      className={`hover:text-blue-300 transition-colors duration-200 group ${className}`}
     >
       {children}{' '}
       <span className="group-hover:ml-1 transition-all duration-200">
